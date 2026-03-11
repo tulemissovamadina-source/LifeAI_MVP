140086e65cffea1f2870e80f3a4524a622138e51
tulemissovamadina-source <tulemissova.madina@gmail.com>
2026-03-12 01:46:22 +0600

    Добавила первый MVP
    Добавила первый MVP

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("LifeAI MVP")
+st.write("Простейший прототип AI-ассистента")
+
+text_note = st.text_area("Напишите заметку:")
+
+if st.button("Сохранить текст"):
+    st.write("Вы написали:", text_note)
+
+audio_file = st.file_uploader("Загрузите аудио (mp3/wav):", type=["mp3", "wav"])
+if audio_file is not None:
+    st.write("Аудио загружено:", audio_file.name)
