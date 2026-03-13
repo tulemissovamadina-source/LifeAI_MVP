6c53d30f0fa404ff1fac36f9ce6f0fcdad9bef44
tulemissovamadina-source <tulemissova.madina@gmail.com>
2026-03-13 17:21:37 +0600

    Update app.py
    настройка с OpenAI

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+from openai import OpenAI
 import streamlit as st
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+        import streamlit as st
 
 st.title("LifeAI MVP")
 st.write("Простейший прототип AI-ассистента")
@@ -10,4 +14,5 @@ if st.button("Сохранить текст"):
 
 audio_file = st.file_uploader("Загрузите аудио (mp3/wav):", type=["mp3", "wav"])
 if audio_file is not None:
+
     st.write("Аудио загружено:", audio_file.name)
